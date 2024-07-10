ac9420396cc1cad63fd72e83a9753781eef59fdd
Younus Ali Akash <younusaliakash8@gmail.com>
2024-07-11 01:30:00 +0600

    header section almost done

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const toggleBtn = document.getElementById("toggle_btn")
 const nav = document.querySelector('.nav');
+const headerLink = document.querySelector('.header_link');
+const brandLogo = document.getElementById("brandLogo")
+const shareLogo = document.getElementById("shareLogo");
 
 toggleBtn.addEventListener("click", () => {
-    document.querySelector('.header_link').classList.toggle('toggle_on')
+    headerLink.classList.toggle('toggle_on')
+
+    if (headerLink.classList.contains('toggle_on')) {
+        toggleBtn.src = '/assets/images/close_black.png';
+    } else {
+        // toggleBtn.src = '/assets/images/hamburger_menu.png';
+        if(nav.classList.contains('active')){
+            toggleBtn.src = '/assets/svg/hamburger.svg'
+        } else {
+            toggleBtn.src = '/assets/images/hamburger_menu.png';
+        }
+    }
 })
 
 window.addEventListener('scroll', fixNav);
 
 function fixNav() {
-    if (window.scrollY > nav.offsetHeight + 150) {
+    if (window.scrollY > nav.offsetHeight + 300) {
         nav.classList.add('active')
+        brandLogo.src = '/assets/images/brand_logo_black.png'
+        if (!headerLink.classList.contains('toggle_on')) {
+            shareLogo.src = '/assets/images/share_black.png'
+            toggleBtn.src = '/assets/svg/hamburger.svg'
+        }
     } else {
         nav.classList.remove('active')
+        brandLogo.src = '/assets/images/brand_logo.png'
+        shareLogo.src = '/assets/images/share.png'
+        if (!headerLink.classList.contains('toggle_on')) {
+            toggleBtn.src = '/assets/images/hamburger_menu.png';
+        }
     }
 }
