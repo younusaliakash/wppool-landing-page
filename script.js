6792a432bfdd74b36f0047f944e14a2f6f696a53
Younus Ali Akash <younusaliakash8@gmail.com>
2024-07-10 21:32:07 +0600

    sixty parcent header done

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggleBtn = document.getElementById("toggle_btn")
+const nav = document.querySelector('.nav');
+
+toggleBtn.addEventListener("click", () => {
+    document.querySelector('.header_link').classList.toggle('toggle_on')
+})
+
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+    if (window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
