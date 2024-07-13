1dce649f7dfe08a6ca8ec1c4dafe5043f0435760
Younus Ali Akash <younusaliakash8@gmail.com>
2024-07-13 20:07:31 +0600

    hamburger menu change

--- script.js
+++ script.js
@@ -5,20 +5,50 @@ const brandLogo = document.getElementById("brandLogo")
 const shareLogo = document.getElementById("shareLogo");
 const tableBody = document.querySelector("#companyTable tbody");
 
-toggleBtn.addEventListener("click", () => {
-    headerLink.classList.toggle('toggle_on')
 
-    if (headerLink.classList.contains('toggle_on')) {
-        toggleBtn.src = '/assets/images/close_black.png';
-    } else {
-        // toggleBtn.src = '/assets/images/hamburger_menu.png';
-        if(nav.classList.contains('active')){
-            toggleBtn.src = '/assets/svg/hamburger.svg'
-        } else {
-            toggleBtn.src = '/assets/images/hamburger_menu.png';
-        }
-    }
+// const hamburgerTop = document.querySelector('.hamburger-top')
+// const hamburgerMiddle = document.querySelector('.hamburger-middle')
+// const hamburgerBottom = document.querySelector('.hamburger-bottom')
+
+
+
+
+
+
+const btn = document.getElementById('menu-btn');
+const navEl = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open')
+    headerLink.classList.toggle('toggle_on')
+    // navEl.classList.toggle('flex')
+    // navEl.classList.toggle('hidden')
+    // if (headerLink.classList.contains('toggle_on')) {
+    //     toggleBtn.src = '/assets/images/close_black.png';
+    // } else {
+    //     // toggleBtn.src = '/assets/images/hamburger_menu.png';
+    //     if(nav.classList.contains('active')){
+    //         toggleBtn.src = '/assets/svg/hamburger.svg'
+    //     } else {
+    //         toggleBtn.src = '/assets/images/hamburger_menu.png';
+    //     }
+    // }
 })
+
+// toggleBtn.addEventListener("click", () => {
+//     headerLink.classList.toggle('toggle_on')
+
+//     if (headerLink.classList.contains('toggle_on')) {
+//         toggleBtn.src = '/assets/images/close_black.png';
+//     } else {
+//         // toggleBtn.src = '/assets/images/hamburger_menu.png';
+//         if(nav.classList.contains('active')){
+//             toggleBtn.src = '/assets/svg/hamburger.svg'
+//         } else {
+//             toggleBtn.src = '/assets/images/hamburger_menu.png';
+//         }
+//     }
+// })
 
 window.addEventListener('scroll', fixNav);
 
@@ -28,15 +58,15 @@ function fixNav() {
         brandLogo.src = '/assets/images/brand_logo_black.png'
         if (!headerLink.classList.contains('toggle_on')) {
             shareLogo.src = '/assets/images/share_black.png'
-            toggleBtn.src = '/assets/svg/hamburger.svg'
+            // toggleBtn.src = '/assets/svg/hamburger.svg'
         }
     } else {
         nav.classList.remove('active')
         brandLogo.src = '/assets/images/brand_logo.png'
         shareLogo.src = '/assets/images/share.png'
-        if (!headerLink.classList.contains('toggle_on')) {
-            toggleBtn.src = '/assets/images/hamburger_menu.png';
-        }
+        // if (!headerLink.classList.contains('toggle_on')) {
+        //     toggleBtn.src = '/assets/images/hamburger_menu.png';
+        // }
     }
 }
 
